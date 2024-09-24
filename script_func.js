bca512fbf6fda7017885ead2beea837e2a51298d
kalamanakalam <61622481+kalamanakalam@users.noreply.github.com>
2024-09-24 12:43:20 +0200

    Add files via upload

--- script_func.js
+++ script_func.js
@@ -35,3 +35,38 @@ function getMathResult(base, mass){
 }
 console.log(getMathResult(10, 0));
 
+function calculateVolumeAndArea(length){
+    if(length != '' && length > 0 && Number.isInteger(length)){
+        let a;
+        a = `Объем куба: ${length * length * length}, площадь всей поверхности: ${6*(length*length)}`;
+        return a;
+    }else{
+        let b = ('При вычислении произошла ошибка');
+        return b;
+    }
+}
+console.log(calculateVolumeAndArea(5.5));
+
+function getCoupeNumber(miesto) {
+    if(miesto !== '' && Number.isInteger(miesto) && miesto > 0 ){
+        if(miesto <= 0 || miesto > 36){
+            const a = "Таких мест в вагоне не существует";
+            return a;
+        }else if(miesto%4 == 0 ){
+            return miesto/4;
+        }else if(miesto%4 == 1){
+            let c = ++miesto/4;
+            c = Math.round(c);
+            return c;
+        }else{
+            let d = miesto/4;
+            d = Math.round(d);
+            return d;
+        }
+    }else {
+        const b = "Ошибка. Проверьте правильность введенного номера места";
+        return b;
+    }
+}
+console.log(getCoupeNumber(5));
+
